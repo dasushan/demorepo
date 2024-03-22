5ce10a43fb0eb5c1ab50c169332531438622b5ee
dasushan <53913896+dasushan@users.noreply.github.com>
2024-03-22 23:14:58 +0530

    adding new file

---
+++ Task1/index.js
@@ -0,0 +1,14 @@
+// Write your code below:
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World'
+mainHeading.style.color =  'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const secondHeading = document.getElementById('basket-heading');
+secondHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
